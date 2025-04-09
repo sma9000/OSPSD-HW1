e7d6a8fc77d5264b9bbc68a84357c891f6c4f43d
sma9000 <sma9000@nyu.edu>
2025-04-09 15:58:17 -0400

    Fix install step: combine apt commands, prevent prompts

--- build/CMakeFiles/test_calculator.dir/src/calculator/calculator.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/test_calculator.dir/src/calculator/calculator.cpp.o: \
-  /Users/cheyenne/Documents/GitHub/OSPSD-HW1/src/calculator/calculator.cpp \
-  /Users/cheyenne/Documents/GitHub/OSPSD-HW1/src/calculator/calculator.h
